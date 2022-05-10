38f3650a9bbbf45f2d41b0383647dce0bc7120c0
andre9653 <andresena9653@gmail.com>
2022-05-09 21:49:00 -0300

    feat: criando estrutura para captura de possiveis erros

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 require('dotenv').config();
 
 const app = express();
 const { PORT, HOST } = process.env;
 app.use(express.json());
+app.use(errorMiddleware);
 
 const server = app.listen(PORT, HOST, () => {
   console.log(`Start server on PORT ${PORT}`);
